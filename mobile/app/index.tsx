cb58c953638dc312fbbcd612df15410921d3fed4
ktbm <boliventtalla12@gmail.com>
2026-02-23 17:05:52 +0100

    begining

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
 
-const YourApp = () => {
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Try editing me! 🎉</Text>
-    </View>
+    <ScrollView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="You can type in me"
+      />
+    </ScrollView>
   );
 };
 
-export default YourApp;
+export default App;
