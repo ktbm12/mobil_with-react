846ab66dd295f6fdb39f3e291f8e6a8be7555323
ktbm <boliventtalla12@gmail.com>
2026-02-23 21:36:12 +0100

    update chart

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import {Text} from 'react-native';
 
-const App = () => {
-  return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
-  );
+const Cat = () => {
+  return <Text>Hello, I am your cat!</Text>;
 };
 
-export default App;
+export default Cat;
