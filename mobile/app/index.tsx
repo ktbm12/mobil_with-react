ee39c296258b6e97ab254baa17a1a778fb880ae3
ktbm <boliventtalla12@gmail.com>
2026-02-24 11:42:23 +0100

    update chart

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,38 +1,32 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
-
-const FlatListBasics = () => {
+const FixedDimensionsBasics = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    <View>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: 'steelblue',
+        }}
       />
     </View>
   );
 };
 
-export default FlatListBasics;
+export default FixedDimensionsBasics;
