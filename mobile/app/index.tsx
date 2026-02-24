2f18d37db93034ed09fe99b34d84cb948ed3d6bb
ktbm <boliventtalla12@gmail.com>
2026-02-24 16:56:01 +0100

    begining

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,40 +1,127 @@
 import React from 'react';
-import {Alert, Button, StyleSheet, View} from 'react-native';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Animated,
+  useWindowDimensions,
+  useAnimatedValue,
+} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-const ButtonBasics = () => {
-  const onPress = () => {
-    Alert.alert('You tapped the button!');
-  };
+const images = new Array(6).fill(
+  'https://images.unsplash.com/photo-1556740749-887f6717d7e4',
+);
+
+const App = () => {
+  const scrollX = useAnimatedValue(0);
+
+  const {width: windowWidth} = useWindowDimensions();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <Button onPress={onPress} title="Press Me" />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button onPress={onPress} title="Press Me" color="#841584" />
-      </View>
-      <View style={styles.alternativeLayoutButtonContainer}>
-        <Button onPress={onPress} title="This looks great!" />
-        <Button onPress={onPress} title="OK!" color="#841584" />
-      </View>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.scrollContainer}>
+          <ScrollView
+            horizontal={true}
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            onScroll={Animated.event([
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: scrollX,
+                  },
+                },
+              },
+            ])}
+            scrollEventThrottle={1}>
+            {images.map((image, imageIndex) => {
+              return (
+                <View
+                  style={{width: windowWidth, height: 250}}
+                  key={imageIndex}>
+                  <ImageBackground source={{uri: image}} style={styles.card}>
+                    <View style={styles.textContainer}>
+                      <Text style={styles.infoText}>
+                        {'Image - ' + imageIndex}
+                      </Text>
+                    </View>
+                  </ImageBackground>
+                </View>
+              );
+            })}
+          </ScrollView>
+          <View style={styles.indicatorContainer}>
+            {images.map((image, imageIndex) => {
+              const width = scrollX.interpolate({
+                inputRange: [
+                  windowWidth * (imageIndex - 1),
+                  windowWidth * imageIndex,
+                  windowWidth * (imageIndex + 1),
+                ],
+                outputRange: [8, 16, 8],
+                extrapolate: 'clamp',
+              });
+              return (
+                <Animated.View
+                  key={imageIndex}
+                  style={[styles.normalDot, {width}]}
+                />
+              );
+            })}
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    margin: 20,
+  scrollContainer: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
+  card: {
+    flex: 1,
+    marginVertical: 4,
+    marginHorizontal: 16,
+    borderRadius: 5,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    backgroundColor: 'rgba(0,0,0, 0.7)',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    borderRadius: 5,
+  },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  normalDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    backgroundColor: 'silver',
+    marginHorizontal: 4,
+  },
+  indicatorContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
-export default ButtonBasics;
+export default App;
