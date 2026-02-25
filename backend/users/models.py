dffd5a4f92fd82c8fe94ce1a2724b10c4e861f6d
ktbm <boliventtalla12@gmail.com>
2026-02-25 12:57:14 +0100

    update

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    name = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+    sub_name= models.CharField(default=False)
+
+    def __str__(self):
+        return self.title
